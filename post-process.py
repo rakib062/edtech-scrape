d68af17f6952e1cd5b5cf298f01fc843b9d9d1ba
rakib062 <rakib_cse_062@yahoo.com>
2021-11-17 19:29:40 +0100

    minor

--- post-process.py
+++ post-process.py
@@ -5,7 +5,7 @@ def combine_dfs(indir, tag, outdir):
 	'''
 	Combine all dataframes for a given hastag to one dataframe.
 	'''
-	dfs = [pd.read_csv(file) for file in \
+	dfs = [pd.read_csv(file, lineterminator='\n') for file in \
 			glob.glob('{}/tweets-search-{}*.csv'.format(indir, tag))]
 
 	print("tweet dataframes:{}, tweets:{}".format(len(dfs), 
@@ -13,18 +13,18 @@ def combine_dfs(indir, tag, outdir):
 	if len(dfs)>0:
 		pd.concat(dfs).to_csv('{}/tweets-search-{}.csv'.format(outdir, tag))
 
-	dfs = [pd.read_csv(file) for file in \
+	dfs = [pd.read_csv(file, lineterminator='\n') for file in \
 			glob.glob('{}/users-search-{}*.csv'.format(indir, tag))]
 	
 	if len(dfs)>0:
 		pd.concat(dfs).to_csv('{}/users-search-{}.csv'.format(outdir, tag))
 
-	dfs = [pd.read_csv(file) for file in \
+	dfs = [pd.read_csv(file, lineterminator='\n') for file in \
 			glob.glob('{}/inc-tweets-search-{}*.csv'.format(indir, tag))]
 	if len(dfs)>0:
 		pd.concat(dfs).to_csv('{}/inc-tweets-search-{}.csv'.format(outdir, tag))
 
-	dfs = [pd.read_csv(file) for file in \
+	dfs = [pd.read_csv(file, lineterminator='\n') for file in \
 			glob.glob('{}/media-search-{}*.csv'.format(indir, tag))]
 	if len(dfs)>0:
 		pd.concat(dfs).to_csv('{}/media-search-{}.csv'.format(outdir, tag))
