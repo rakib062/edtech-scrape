efba86ed0b874506891272500ef7ec6743857a59
rakib062 <rakib_cse_062@yahoo.com>
2021-11-27 21:17:13 +0100

    tweet stat generator

--- post-process.py
+++ post-process.py
@@ -22,7 +22,8 @@ def combine_dfs(indir, tag, outdir, stat_dir):
 
 	df['tweetid'] = df.tweetid.astype(int)
 	latest_tweet = df[df.tweetid>=df.tweetid.max()].iloc[0]
-	
+	print(latest_tweet)
+	print(latest_tweet.to_dict())
 	with open('{}/tweet-stat-{}.json'.format(stat_dir, tag), 'w') as fp:
 		json.dump(latest_tweet.to_dict(), fp)
 	# with open('{}/tweet-stat-{}.csv'.format(stat_dir, tag), 'w') as csv_file:
