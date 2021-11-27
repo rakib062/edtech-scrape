460670fbbbf883baa4c9f5cfcc400291ba849514
rakib062 <rakib_cse_062@yahoo.com>
2021-11-27 21:08:07 +0100

    tweet stat generator

--- post-process.py
+++ post-process.py
@@ -1,22 +1,31 @@
 import pandas as pd
 import glob, sys, csv
 
-def combine_dfs(indir, tag, outdir):
+def combine_dfs(indir, tag, outdir, stat_dir):
 	'''
 	Combine all dataframes for a given hastag to one dataframe.
 	'''
+
 	print("Tag: ", tag)
 	dfs = [pd.read_csv(file, lineterminator='\n') for file in \
 			glob.glob('{}/tweets-search-{}*.csv'.format(indir, tag))]
 	if len(dfs)==0:    
 		print("No data found.")
 		return
-
-
 	print("tweet dataframes:{}, tweets:{}, combined shape:{}".format(len(dfs), 
 		sum([len(df) for df in dfs]), pd.concat(dfs).shape))
-	if len(dfs)>0:
-		pd.concat(dfs).to_csv('{}/tweets-search-{}.csv'.format(outdir, tag))
+
+	df = pd.concat(dfs)
+	df['tweetid'] = df.tweetid.astype(str)
+	df = df[df.tweetid!='nan']
+	df.to_csv('{}/tweets-search-{}.csv'.format(outdir, tag))
+
+	df['tweetid'] = df.tweetid.astype(int)
+	latest_tweet = df[df.tweetid>=df.tweetid.max()].iloc[0]
+	with open('{}/tweet-stat-{}.csv'.format(stat_dir, tag), 'w') as csv_file:
+    	writer = csv.writer(csv_file)
+    	writer.writerow(latest_tweet.to_dict())
+
 
 	dfs = [pd.read_csv(file, lineterminator='\n') for file in \
 			glob.glob('{}/users-search-{}*.csv'.format(indir, tag))]
@@ -27,17 +36,24 @@ def combine_dfs(indir, tag, outdir):
 	dfs = [pd.read_csv(file, lineterminator='\n') for file in \
 			glob.glob('{}/inc-tweets-search-{}*.csv'.format(indir, tag))]
 	if len(dfs)>0:
-		pd.concat(dfs).to_csv('{}/inc-tweets-search-{}.csv'.format(outdir, tag))
+		df = pd.concat(dfs)
+		df['tweetid'] = df.tweetid.astype(str)
+		df = df[df.tweetid!='nan']
+		df.to_csv('{}/inc-tweets-search-{}.csv'.format(outdir, tag))
 
 	dfs = [pd.read_csv(file, lineterminator='\n') for file in \
 			glob.glob('{}/media-search-{}*.csv'.format(indir, tag))]
 	if len(dfs)>0:
-		pd.concat(dfs).to_csv('{}/media-search-{}.csv'.format(outdir, tag))
+		df = pd.concat(dfs)
+		df['tweetid'] = df.tweetid.astype(str)
+		df = df[df.tweetid!='nan']
+		df.to_csv('{}/media-search-{}.csv'.format(outdir, tag))
 
 
 indir = sys.argv[1]
 tag_file = sys.argv[2]
 outdir = sys.argv[3]
+stat_dir=sys.argv[4]
 
 tags = set([])
 with open(tag_file) as csv_file:
@@ -45,4 +61,4 @@ with open(tag_file) as csv_file:
 	tags = set(list(reader)[0])
 
 for tag in tags:
-	combine_dfs(indir, tag, outdir)
+	combine_dfs(indir, tag, outdir, stat_dir)
