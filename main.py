bdd535a29ffefbed37e5833472dc4873a85fc0ee
rakib062 <rakib_cse_062@yahoo.com>
2021-11-30 00:34:24 +0100

    adding mentions file

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sys, csv,os
 import collect_tweets
 
 outdir = sys.argv[1]
+tagfile=sys.argv[2]
 if not os.path.exists(outdir):
         os.makedirs(outdir)
         
-with open('tags.csv') as csv_file:
+with open(tagfile) as csv_file:
     reader = csv.reader(csv_file)
     tags = set(list(reader)[0])
 
@@ -27,7 +28,20 @@ for tag in tags:
     tag = tag.strip()
     print("Starting search for tag no:{} of {}, tag:{}".format(i, len(tags),tag))
     i+=1
-    collect_tweets.search_tweets(tag+" -is:retweet", outdir, i)
+    '''
+    Retrieve the latest tweet collected before
+    '''
+    start_time = '2006-03-21T00:00:00Z'
+    since_id = None
+    if os.path.isfile('{}/tweet-stat-{}.json'.format(stat_dir, tag)):
+        with open('{}/tweet-stat-{}.json'.format(stat_dir, tag), 'r') as fp:
+            tweet = json.loads(json.load((latest_tweet.to_json(), fp)))
+            since_id = tweet.tweetid
+
+
+    collect_tweets.search_tweets(tag+" -is:retweet", 
+                                start_time, since_id,outdir, i)
+
     donetags.add(tag)
     with open('donetags.csv', 'w') as csv_file:  
         writer = csv.writer(csv_file)
