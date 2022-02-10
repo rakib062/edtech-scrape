bde14bec15a20ca4a8c79bd7855d2a643123b91f
rakib062 <rakib_cse_062@yahoo.com>
2022-02-10 16:21:50 -0700

    pyenv installation

---
+++ pyenv.sh
@@ -0,0 +1,25 @@
+#Install pyenv for version management
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+# Follow the instruction to modify ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+exec "$SHELL"
+
+
+# Install the latest Python from source code
+pyenv install 3.9.10
+
+# Check installed Python versions
+pyenv versions
+
+# Switch Python version
+pyenv global 3.9.10
+
+# Check where Python is actually installed
+pyenv prefix
+# Check the current Python version
+python -V
