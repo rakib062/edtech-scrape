d7273f4de97704be3ebadd6c7d386722da2f5602
Rakibul Hasan <rhasan3@oked3637.3500.dhcp.asu.edu>
2022-02-16 15:06:44 -0700

    using argparse for tweet collection

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys, csv,os
 import collect_tweets
 import json
 import argparse
-import combine_dataframes
+# import combine_dataframes
 
 
 
@@ -55,35 +55,13 @@ def collect_tweets(outdir, tagfile, stat_dir):
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(__doc__)
-    parser.add_argument("--task", type=str, choices=["collect-tweet", "combine-csv-dfs", "merge-dfs"], help='what do you want to do?')
-    parser.add_argument("--outdir", type=str, default='', help='out put directory')
-    parser.add_argument( "--indir", type=str, default='', help="input directory")
-    # parser.add_argument( "--outfile", type=str, default='', help="output file for merged data frames")
-    parser.add_argument( "--kwfile", type=str, help="files containig search keywords/hashtags/...")
-    parser.add_argument( "--statdir", type=str, default='', help="tweet stat dir")
-    parser.add_argument("--rerank", type=str, choices=["tf", "tfidf", "tfdf", "graph"])
-
-    # parser.add_argument("--clustering_algo", type=str, required=True, choices=["KMeans", "SPKMeans", "GMM", "KMedoids","Agglo","DBSCAN","Spectral","VMFM",
-    #     'from_file', 'LDA'])
-
-    # parser.add_argument( "--topics_file", type=str, help="topics file")
-
-    # parser.add_argument('--use_dims', type=int)
-    # parser.add_argument('--num_topics',  nargs='+', type=int, default=[20])
-    # parser.add_argument("--doc_info", type=str, choices=["SVD", "DUP", "WGT", "robust", \
-    # "logtfdf"])
-     
-
-    # parser.add_argument('--id2name', type=Path, help="id2name file")
-
-    # parser.add_argument("--dataset", type=str, default ="20NG", choices=["20NG", "children", "reuters"])
-
-    # parser.add_argument("--preprocess", type=int, default=5)
+    parser.add_argument("--task", type=str, required=True, choices=["collect-tweet", "combine-csv-dfs", "merge-dfs"], help='what do you want to do?')
+    parser.add_argument("--out_dir", required=True, type=str, default='', help='out put directory')
+    parser.add_argument( "--in_dir", type=str, default='', help="input directory")
+    parser.add_argument( "--kw_file", type=str, help="files containig search keywords/hashtags/...")
+    parser.add_argument( "--stat_dir", type=str, default='', help="tweet stat dir")
     
-    parser.add_argument("--vocab", required=True,  type=str, nargs='+', default=[])
-    # parser.add_argument("--scale", type=str, required=False)
-
-
+  
     args = parser.parse_args()
     return args
 
@@ -92,6 +70,8 @@ def parse_args() -> argparse.Namespace:
 if __name__ == "__main__":
     args = parse_args()
 
-    if args.task=='merge-dfs':
-        combine_dataframes.merge_tweet_dfs(indir=args.indir, tagfile=args.kwfile, outdir=args.outdir)
+    # if  args.task=='merge-dfs':
+    #     combine_dataframes.merge_tweet_dfs(indir=args.indir, tagfile=args.kwfile, outdir=args.outdir)
+    if args.task=='collect-tweet':
+        collect_tweets(args.out_dir, args.kw_file, args.stat_dir)
 
